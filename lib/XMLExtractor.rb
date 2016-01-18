ef0f32288036c4148f86bb637a1e1030594e5a6c
DJia <jiawei.dong@autodesk.com>
2016-01-18 10:59:23 +0800

    local test

--- lib/XMLExtractor.rb
+++ lib/XMLExtractor.rb
@@ -2,15 +2,18 @@ require 'extractor/version'
 
 module XMLExtractor
   def self.extract(input, output)
+  	puts '====================================================='
     # Return if source and target file is not existed.
     return if input.nil? || output.nil?
 
+  	puts 'Check file'
     # Check file
     return unless File.exist?(input)
     return if File.exist?(output)
 
     file_dir = File.dirname(__FILE__)
-    `export LD_LIBRARY_PATH="#{file_dir}/extractor:$LD_LIBRARY_PATH" && #{file_dir}/extractor/Extractor -i #{input} -o #{output} >/dev/null 2>&1`
-
+  	puts "file_dir = #{file_dir}"
+    `export LD_LIBRARY_PATH="#{file_dir}/extractor:$LD_LIBRARY_PATH" && #{file_dir}/extractor/Extractor -i #{input} -o #{output}`
+  	puts '====================================================='
   end
 end

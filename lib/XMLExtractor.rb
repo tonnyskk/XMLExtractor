f1926b04920eede92061c3d89e54a422f605436e
DJia <jiawei.dong@autodesk.com>
2016-01-18 10:45:33 +0800

    Fix lib path issue

--- lib/XMLExtractor.rb
+++ lib/XMLExtractor.rb
@@ -2,10 +2,15 @@ require 'extractor/version'
 
 module XMLExtractor
   def self.extract(input, output)
+    # Return if source and target file is not existed.
     return if input.nil? || output.nil?
-    puts '=========================================='
-    puts "#{Dir.pwd}"
-    `cd .. && export LD_LIBRARY_PATH="XMLExtractor/lib/extractor:$LD_LIBRARY_PATH" && XMLExtractor/lib/extractor/Extractor -i #{input} -o #{output}`
-    puts '=========================================='
+
+    # Check file
+    return unless File.exist?(input)
+    return if File.exist?(output)
+
+    file_dir = File.dirname(__FILE__)
+    `export LD_LIBRARY_PATH="#{file_dir}/extractor:$LD_LIBRARY_PATH" && #{file_dir}/extractor/Extractor -i #{input} -o #{output} >/dev/null 2>&1`
+
   end
 end
